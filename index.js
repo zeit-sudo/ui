a696ce7513560d516c30d013651238d95c169456
Andy Chen <andycandomore@gmail.com>
2019-06-01 22:04:50 -0700

    add latest

--- index.js
+++ index.js
@@ -1,14 +1,93 @@
-const { withUiHook } = require('@zeit/integration-utils')
+const { withUiHook, htm } = require('@zeit/integration-utils')
+const UserModel = require('./model');
+const mongoose = require('mongoose');
+const axios = require('axios');
 
-let count = 0
+mongoose.connection.once('open', () => console.log("Mongoose connection opened on process " + process.pid));
+mongoose.Promise = global.Promise;
 
-module.exports = withUiHook(({ payload }) => {
-  console.log('PAYLOAD: ', payload)
-  count += 1
-  return `
-    <Page>
-      <P>Counter: ${count}</P>
-      <Button>Count Me</Button>
-    </Page>
-  `
+module.exports = withUiHook(async({ payload }) => {
+  // console.log(payload);
+  const user_id = payload.user.id;
+  let team_id;
+  if(payload.team && payload.team.id) {
+    team_id = payload.team.id;
+  }
+  await conntectMongo();
+  const user = await UserModel.findOne({user_id});
+  if(!user){
+    const user = new UserModel({user_id, team_id});
+    const savedUser = await user.save();
+    return start({user: savedUser, payload});
+  }else{
+    // console.log(user);
+    return start({user, payload});
+  }
 })
+
+async function start({user, payload}) {
+  let secretsList = [];
+  const { token } = payload;
+  axios.defaults.baseURL = 'https://api.zeit.co/';
+  axios.defaults.headers.common = {'Authorization': `Bearer ${token}`};
+  if(payload.projectId){
+    const secrets = await axios.get(`v2/now/secrets?teamId=${user.team_id}`);
+    console.log(secrets.data.secrets);
+    return htm`
+    <Page>
+      <Fieldset>
+        <FsContent>
+          <H1>Choose your project:</H1>
+          <ProjectSwitcher />
+        </FsContent>
+      </Fieldset>
+      <Fieldset>
+        <FsContent>
+        <H1>Currently Secrets:</H1>
+        ${secrets.data.secrets.map((s, i)=>{
+          return htm`
+            <Box display="flex" justifyContent="space-between">
+              <Box width="30%">
+                <B>${s.name}</B>
+              </Box>
+              <Box width="30%">
+                <Input name="${s.name}" value="************" />
+              </Box>
+              <Box width="20%">
+                <Button>Delete</Button>
+              </Box>
+              <Box width="20%">
+                <Button>Delete</Button>
+              </Box>
+            </Box>
+          `
+        })}
+        </FsContent>
+      </Fieldset>
+    </Page>
+    `
+  }
+  return htm`
+  <Page>
+    <Fieldset>
+      <FsContent>
+        <H1>Choose your project:</H1>
+        <ProjectSwitcher />
+      </FsContent>
+      <FsContent>
+      </FsContent>
+    </Fieldset>
+  </Page>
+  `
+}
+
+
+function conntectMongo() {
+  return new Promise( (res, rej)=>mongoose.connect('mongodb://localhost/zeit-sudo-ui-hook', (err)=>{
+    if(err){
+      rej(err);
+    }else{
+      res();
+    };
+  }));
+}
--- model.js
+++ model.js
@@ -1,11 +1,11 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// // // Define the model
-// // const Schema = new mongoose.Schema({
-// //     user_id: String,
-// //     team_id: String,
-// //     project_id: String
-// // })
+// Define the model
+const Schema = new mongoose.Schema({
+    user_id: String,
+    team_id: String,
+    project_id: String
+})
 
-// // Export the model
-// module.exports = mongoose.model('User', Schema);
+// Export the model
+module.exports = mongoose.model('User', Schema);
